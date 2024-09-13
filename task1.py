f7bfe49407d5bb125ba9cdcfda89b83f375a2f3f
Sindik <sindereve850live@gmail.com>
2024-09-13 12:35:15 +0500

    save task

---
+++ task1.py
@@ -0,0 +1,11 @@
+from random import random
+import math
+
+input_value = int(input())
+sum_value = input_value 
+
+while input_value !=0:
+    input_value = int(input())
+    sum_value += input_value
+
+print(sum_value)
